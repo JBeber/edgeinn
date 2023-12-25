b6663da78112afd8206d1a258993b5e6e14ead16
Jeremiah Beber <jeremiah.beber@gmail.com>
2023-12-25 14:43:32 -0500

    update css

--- Home.py
+++ Home.py
@@ -23,8 +23,11 @@ def run():
   # Add custom CSS to hide the GitHub icon
   hide_github_icon = """
   <style>
-  #MainMenu {visibility: hidden;}
-  #GithubIcon {visibility: hidden;}
+  .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+  .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+  .viewerBadge_text__1JaDK {
+      display: none;
+  }
   </style>
   """
   st.markdown(hide_github_icon, unsafe_allow_html=True)
