5319028da554bf9db8177722ccfddcc850c9574e
Jeremiah Beber <jeremiah.beber@gmail.com>
2023-12-25 15:38:15 -0500

    added responsive UI width

--- Home.py
+++ Home.py
@@ -14,23 +14,30 @@
 
 import streamlit as st
 import streamlit.components.v1 as components
+from streamlit_dimensions import st_dimensions as dim
 
 def run():
   st.set_page_config(
-      page_title="Edge Inn Schedule"
+      page_title="Edge Inn Schedule",
+      layout='wide'
   )
 
-  # Add custom CSS to hide the GitHub icon
-  hide_github_icon = """
+  # Add custom CSS to hide the toolbar items at the top of the page  
+  hide_toolbar_items = """
   <style>
   #MainMenu {visibility: hidden;}
   #GithubIcon {visibility: hidden;}
   </style>
   """
-  st.markdown(hide_github_icon, unsafe_allow_html=True)
+  st.markdown(hide_toolbar_items, unsafe_allow_html=True)
 
-  iframe_src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=ZjA2MTc0MmYzZTMxNjMxMzdjNjNmYzQ3NzUzMWI5MTBkNDFkZGYxMGNjMzFjYmY0MTNjY2VmZTNmNmI0ZGU1MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23B39DDB"
-  components.iframe(iframe_src, width=800, height=600, scrolling=True)
+  col1, col2, col3 = st.columns([0.05, 0.9, 0.05])
+
+  with col2:
+    iframe_src="https://calendar.google.com/calendar/embed?wkst=1&bgcolor=%23ffffff&ctz=America%2FNew_York&src=ZjA2MTc0MmYzZTMxNjMxMzdjNjNmYzQ3NzUzMWI5MTBkNDFkZGYxMGNjMzFjYmY0MTNjY2VmZTNmNmI0ZGU1MUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23B39DDB"
+    dim_output = dim(key='col2')
+    width = dim_output["width"]
+    components.iframe(iframe_src, width=width, height= width*0.7, scrolling=True)
 
 
 if __name__ == "__main__":
