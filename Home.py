06c6925a5da3e7052bf61bb5605051f69f7f24b2
Jeremiah Beber <jeremiah.beber@gmail.com>
2023-12-26 15:24:06 -0500

    fixed password implementation

--- Home.py
+++ Home.py
@@ -17,38 +17,37 @@ import streamlit.components.v1 as components
 import hmac
 # from streamlit_dimensions import st_dimensions as dim
 
-# def check_password():
-#     """Returns `True` if the user had the correct password."""
-
-#     def password_entered():
-#         """Checks whether a password entered by the user is correct."""
-#         if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
-#             st.session_state["password_correct"] = True
-#             del st.session_state["password"]  # Don't store the password.
-#         else:
-#             st.session_state["password_correct"] = False
-
-#     # Return True if the passward is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
-
-#     # Show input for password.
-#     st.text_input(
-#         "Password", type="password", on_change=password_entered, key="password"
-#     )
-#     if "password_correct" in st.session_state:
-#         st.error("😕 Password incorrect")
-#     return False
-
-
-# if not check_password():
-#     st.stop()  # Do not continue if check_password is not True.
-
 def run():
   st.set_page_config(
       page_title="Edge Inn Schedule",
       layout='wide'
   )
+
+  def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the passward is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+  if not check_password():
+      st.stop()  # Do not continue if check_password is not True.
 
   # Add custom CSS to hide the toolbar items at the top of the page  
   hide_toolbar_items = """
